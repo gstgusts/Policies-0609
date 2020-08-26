db21978ebae80161bb105dfa8fe511f8c149ff78
Gusts Linkevics <gusts@gst.lv>
2020-08-26 21:24:46 +0300

    Policy repository added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,59 @@
 package com.company;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
+    private static PolicyRepository policyRepository = new PolicyRepository();
+    private static JPanel mainPanel;
+    private static JList<String> policyList;
+    private static JButton btnCalculate;
+    private static JLabel lblCalculationResult;
+
     public static void main(String[] args) {
 
+        policyRepository.loadPolicies();
 
+        JFrame mainFrame = new JFrame("Policy premium calculator");
+        mainFrame.setSize(500, 500);
+        mainFrame.setResizable(false);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(3,1));
+
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+
+        for (var policy:
+             policyRepository.getPolicies()) {
+             l1.addElement(policy.getPolicyNumber());
+        }
+
+        policyList = new JList<>(l1);
+        mainPanel.add(policyList);
+
+        lblCalculationResult = new JLabel();
+
+        btnCalculate = new JButton("Calculate");
+        btnCalculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                var selectedPolicyNumber = policyList.getSelectedValue();
+                if(selectedPolicyNumber != null && !selectedPolicyNumber.isEmpty()) {
+                    var premium = policyRepository.calculatePremium(selectedPolicyNumber);
+                    lblCalculationResult.setText(premium + "");
+                }
+            }
+        });
+
+        mainPanel.add(btnCalculate);
+        mainPanel.add(lblCalculationResult);
+
+        mainFrame.setContentPane(mainPanel);
+        mainFrame.setVisible(true);
 
     }
 }
